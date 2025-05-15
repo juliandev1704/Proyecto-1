3248cc69f72365bac5fce19eafedd111fbb2289b
sebastian-gfrd <sc.martineza12@uniandes.edu.co>
2025-05-15 15:19:15 -0500

    Ajustes en administrador, (faltan ajustes atraccion), creac admin test

--- Proyecto1/src/mundo/usuario/Admin.java
+++ Proyecto1/src/mundo/usuario/Admin.java
@@ -1,14 +1,29 @@
 package usuario;
+import java.time.LocalDate;
+
+import atracciones.Atraccion;
 import atracciones.Turno;
+import parque.Parque;
 
 public class Admin extends Usuario {
-
-    public Admin(String userName, String password, String correo) {
+	private Parque parque;
+    public Admin(String userName, String password, String correo, Parque parque) {
         super(userName, password, correo);
+        parque = this.parque;
     }
 
-    public void crearNuevaAtraccion(/*..., ParqueDiversiones parque*/) {
+    public void crearNuevaAtraccion(String nombre, int capacidadMax, int empleadosMin, String ubicacion, int nivel,
+    		boolean esDeTemporada, String fecha_inicial, String fecha_final, String clima) {
         System.out.println("Admin " + super.getUserName() + " está creando una atracción...");
+        Atraccion atraccion = new Atraccion(nombre, capacidadMax, empleadosMin, ubicacion, nivel, true, LocalDate.parse(fecha_inicial), 
+        		LocalDate.parse(fecha_final), clima);
+        
+        //Pendiente de cambios, cambio en parametros del metodo constructor Atraccion (Sara).
+    }
+    public void crearNuevaAtraccion(String nombre, int capacidadMax, int empleadosMin, String ubicacion, int nivel, String clima) {
+        System.out.println("Admin " + super.getUserName() + " está creando una atracción...");
+        Atraccion atraccion = new Atraccion(nombre, capacidadMax, empleadosMin, ubicacion, nivel, false, clima);
+        
     }
 
     public void asignarTurnoAEmpleado(Empleado empleado, Turno turno /*, ParqueDiversiones parque*/) {
--- Proyecto1/src/tests/usuarios/test/AdminTest.java
+++ Proyecto1/src/tests/usuarios/test/AdminTest.java
@@ -27,4 +27,5 @@ public class AdminTest{
 	void agregarTurnoTest() {
 		admin.asignarTurnoAEmpleado(empleado,turno);
 	}
+	
 }
